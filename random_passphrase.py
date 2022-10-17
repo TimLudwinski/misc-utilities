ce73da03112e5dbce3906cdd92c4cd66aeef8105
Tim Ludwinski <TimLudwinski@users.noreply.github.com>
2022-10-17 12:50:52 -0400

    Update random_passphrase.py

--- random_passphrase.py
+++ random_passphrase.py
@@ -27,18 +27,21 @@ Seven Random Words (58000^5)
 """
 import random
 import argparse
+import string
 
 SYS_RAND = random.SystemRandom()
 
-WORD_LIST_FILE = "all_words_alpha.txt" # This contains way too many obscure words
-WORD_LIST_FILE = "corncob_lowercase_word_list.txt" # This contains less obscure words (but still a lot of compound words and obscure stuff)
-with open(WORD_LIST_FILE) as f:
-    WORD_LIST = [ word.strip() for word in f ]
+# WORD_LIST_FILE = "all_words_alpha.txt" # This contains way too many obscure words
+# WORD_LIST_FILE = "corncob_lowercase_word_list.txt" # This contains less obscure words (but still a lot of compound words and obscure stuff)
 
-def get_random_password(num_words, sep=" "):
+def get_word_list(filename):
+    with open(filename) as f:
+        return [ word.strip() for word in f ]
+
+def get_random_password(num_words, word_list, sep=" "):
     wordL = []
     for _i in range(num_words):
-        wordL.append(SYS_RAND.choice(WORD_LIST))
+        wordL.append(SYS_RAND.choice(word_list))
 
     return sep.join(wordL)
 
@@ -48,9 +51,13 @@ if __name__ == "__main__":
     parser.add_argument('--separator', type=str, default=' ', help='')
     parser.add_argument('--print-entropy',  action='store_true', help='Display the complexity of the password')
     parser.add_argument('--num-passphrases', type=int, default=1, help='Generate and Print multiple pass phrases')
+    parser.add_argument('--word-list', type=str, default="corncob_lowercase_word_list.txt", help='The word list to use')
     args = parser.parse_args()
     
+    word_list = get_word_list(args.word_list)
+    # word_list = string.ascii_lowercase + string.ascii_uppercase + string.digits + "!#$%^&*()" # Letters, numbers and specials as word list
+    
     if args.print_entropy:
-        print("Entropy: {0:,}".format(len(WORD_LIST) ** args.num_words))
+        print("Entropy: {0:,}".format(len(word_list) ** args.num_words))
     for _ in range(args.num_passphrases):
-        print("Pass Phrase: {0}".format(get_random_password(args.num_words, args.separator)))
+        print("Pass Phrase: {0}".format(get_random_password(args.num_words, word_list, args.separator)))
